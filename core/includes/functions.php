b142c6c54928c07004b17e0f632489b370515c62
Charly Escalona <charly.escalona1@gmail.com>
2020-08-29 20:54:17 +0200

    affichage des images "Autres"

--- core/includes/functions.php
+++ core/includes/functions.php
@@ -2,30 +2,55 @@
 
 function get_by($tag)
 {
-    $files = scandir('pictures/' . $tag);
-    $files = array_diff($files, array("..", ".", '.DS_Store'));
-    return $files;
+    if($tag == "others"){
+        $files_array = scandir('pictures');
+        $files_array = array_diff($files_array, array("..", ".", '.DS_Store'));
+        $files = array();
+        foreach($files_array as $file){
+            if(is_file('pictures/' . $file) && !is_dir('pictures/' . $file)){
+                array_push($files, $file);
+            } else if(is_dir('pictures/' . $file) && !is_file('pictures/' . $file)) {}
+        }  
+        return $files;
+    } else {
+        $files = scandir('pictures/' . $tag);
+        $files = array_diff($files, array("..", ".", '.DS_Store'));
+        return $files;
+    }
 }
 
 function get_all(){
-    $folders = scandir('pictures');
-    $folders = array_diff($folders, array("..", ".", '.DS_Store'));
-    $files = array();
-    foreach($folders as $folder){
-        $files_divided = scandir('pictures/' . $folder);
-        $files_divided = array_diff($files_divided, array("..", ".", '.DS_Store'));
-        foreach($files_divided as $file){
-            array_push($files, $folder . '/' . $file);
-        }
+    $files = scandir('pictures');
+    $files = array_diff($files, array("..", ".", '.DS_Store'));
+    $files_array = array();
+    $test = array();
+    foreach($files as $file){
+        $files_divided = scandir('pictures/' . $file);
+            $files_divided = array_diff($files_divided, array("..", ".", '.DS_Store'));
+            foreach($files_divided as $folder){
 
+                if(is_file('pictures/' . $folder)){
+                    array_push($files_array, $file);
+                } else if(is_dir('pictures/' . $folder)) {
+                    array_push($test, $file . '/' . $folder);
+                }
+            }
+           
     }
-    return $files; 
+
+    var_dump($test);
 }
 
 function loop_pictures($datas, $tag = ""){
+   if($tag == "others"){
+    foreach($datas as $data){
+        echo "<img src='pictures/". $data ."'>";
+    }
+   } else {
     foreach($datas as $data){
         echo "<img src='pictures/" . $tag ."/". $data ."'>";
     }
+   }
 }
 
 function get_tags(){
@@ -34,4 +59,6 @@ function get_tags(){
     foreach($folders as $folder){
         echo '<li><a class="nav_link" id="' . $folder .'" href="index.php?p=home&type=' . $folder .'">' . $folder .'</a></li>';
     }
+    echo '<li><a class="nav_link" id="others" href="index.php?p=home&type=others">Autres</a></li>';
 }
+

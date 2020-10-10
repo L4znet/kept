0b49dbb7b0ef97283f654b2b933b6762d1d3b789
Charly Escalona <charly2221@hotmail.fr>
2020-10-10 20:24:02 +0200

    Add ob_start func for fix
    Ajout de la fonction ob_start et modification de l'alignement

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
 require 'core/includes/functions.php';
-    if(isset($_GET['p'])){
-        $p = $_GET['p'];
-    } else {
-        $p = "home";
-    }
+if (isset($_GET['p'])) {
+    $p = $_GET['p'];
+} else {
+    $p = "home";
+}
 
 
-if(file_exists('pages/' . $p . '.php')){
+if (file_exists('pages/' . $p . '.php')) {
+    ob_start();
     require('pages/' . $p . '.php');
     $content = ob_get_clean();
     require 'pages/templates/default.php';
 } else {
+    ob_start();
     require('pages/404.php');
     $content = ob_get_clean();
     require 'pages/templates/404_template.php';
